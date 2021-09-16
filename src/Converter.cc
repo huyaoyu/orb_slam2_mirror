b9f7de73be783ef90cdd23996ec3c95b8cc022ab
yaoyuh on cluster <yaoyuh@andrew.cmu.edu>
2021-09-16 11:54:21 -0400

    Dummy.

--- src/Converter.cc
+++ src/Converter.cc
@@ -26,6 +26,7 @@ namespace ORB_SLAM2
 
 std::vector<cv::Mat> Converter::toDescriptorVector(const cv::Mat &Descriptors)
 {
+    //std::vector<cv::Mat> vDesc;
     std::vector<cv::Mat> vDesc;
     vDesc.reserve(Descriptors.rows);
     for (int j=0;j<Descriptors.rows;j++)
